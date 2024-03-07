7f877b38ba5aa540ad912c8eb3986ed65bb8b30b
GIMME <3086660647@qq.com>
2024-03-07 17:47:44 +0800

    项目初始化（模块创建）

---
+++ admin/src/main/java/org/jia/mylink/admin/MyLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package org.jia.mylink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  admin模块启动类
+ *
+ * @author JIA
+ */
+
+@SpringBootApplication
+public class MyLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyLinkAdminApplication.class,args);
+    }
+}
