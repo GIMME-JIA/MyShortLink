71e3da44827157b45351e463d924263ab7fd43d9
GIMME <3086660647@qq.com>
2024-03-12 11:47:38 +0800

    feature：开发后台管理系统->创建服务层常量类

---
+++ admin/src/main/java/org/jia/mylink/admin/common/constant/ServiceConstant.java
@@ -0,0 +1,15 @@
+package org.jia.mylink.admin.common.constant;
+
+/**
+ * 服务层常量类
+ * @author JIA
+ * @version 1.0
+ * @since 2024/3/12
+ */
+
+public class ServiceConstant {
+
+    public static final Integer GROUP_MAX_NUMBER = 20;
+
+    public static final String OUT_OF_GROUP_MAX_NUM = "已超出最大分组数:%d";
+}
