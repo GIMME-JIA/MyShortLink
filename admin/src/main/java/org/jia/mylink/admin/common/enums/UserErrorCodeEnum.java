9baa829d6c753225ab38f5e80217ae555d13516d
GIMME <3086660647@qq.com>
2024-03-13 19:43:30 +0800

    fix:更改后台管理系统->用户异常状态码纠正

--- admin/src/main/java/org/jia/mylink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/org/jia/mylink/admin/common/enums/UserErrorCodeEnum.java
@@ -15,43 +15,43 @@ public enum UserErrorCodeEnum implements IErrorCode {
     /**
      * 用户记录不存在
      */
-    USER_NULL("B000200", "用户记录不存在"),
+    USER_NULL("A000200", "用户记录不存在"),
 
     /**
      * 用户记录已存在
      */
-    USER_EXIST("B000201", "用户记录已存在"),
+    USER_EXIST("A000201", "用户记录已存在"),
 
     /**
      * 用户记录新增失败
      */
-    USER_SAVE_ERROR("B000202", "用户记录新增失败"),
+    USER_SAVE_ERROR("A000202", "用户记录新增失败"),
 
     /**
      * 用户名已存在
      */
-    USER_NAME_EXIST("B000205", "用户名已存在"),
+    USER_NAME_EXIST("A000205", "用户名已存在"),
 
     /**
      * 用户名不存在
      */
-    USER_NAME_NOT_EXIST("B000206", "用户名不存在"),
+    USER_NAME_NOT_EXIST("A000206", "用户名不存在"),
 
 
     /**
      * 账号或密码错误
      */
-    USER_LOGIN_ERROR("B000208", "账号或密码错误"),
+    USER_LOGIN_ERROR("A000208", "账号或密码错误"),
 
     /**
      * 用户已登录
      */
-    USER_HAS_LOGIN("B000210", "用户已登录"),
+    USER_HAS_LOGIN("A000210", "用户已登录"),
 
     /**
      * 用户未登录
      */
-    USER_NOT_LOGIN("B000211", "用户未登录");
+    USER_NOT_LOGIN("A000211", "用户未登录");
 
 
 
