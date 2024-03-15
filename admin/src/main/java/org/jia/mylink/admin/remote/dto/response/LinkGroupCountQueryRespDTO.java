0c2ddc9e41af1a2efa2abb6e806ca0a721ef1012
GIMME <3086660647@qq.com>
2024-03-15 19:56:25 +0800

    feature：新增数据参数对象

---
+++ admin/src/main/java/org/jia/mylink/admin/remote/dto/response/LinkGroupCountQueryRespDTO.java
@@ -0,0 +1,22 @@
+package org.jia.mylink.admin.remote.dto.response;
+
+import lombok.Data;
+
+/**
+ * 短链接分组查询响应对象
+ * @author JIA
+ * @version 1.0
+ * @since 2024/3/14
+ */
+@Data
+public class LinkGroupCountQueryRespDTO {
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 短链接数量
+     */
+    private Integer shortLinkCount;
+}
