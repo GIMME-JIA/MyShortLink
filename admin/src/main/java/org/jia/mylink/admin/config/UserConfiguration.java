49c641ddd21f33b9c47fc63789a0bfbd95e20126
GIMME <3086660647@qq.com>
2024-03-21 21:44:54 +0800

    feature:使用过滤器控制请求，最大qps为20

--- admin/src/main/java/org/jia/mylink/admin/config/UserConfiguration.java
+++ admin/src/main/java/org/jia/mylink/admin/config/UserConfiguration.java
@@ -1,9 +1,12 @@
 package org.jia.mylink.admin.config;
 
+import org.jia.mylink.admin.common.biz.UserFlowRiskControlFilter;
 import org.jia.mylink.admin.common.biz.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * 用户配置自动装配
@@ -22,6 +25,21 @@ public class UserConfiguration {
         registration.setFilter(new UserTransmitFilter());
         registration.addUrlPatterns("/*");
         registration.setOrder(0);
+        return registration;
+    }
+
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
         return registration;
     }
 
